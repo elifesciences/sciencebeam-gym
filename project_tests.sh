e0112d1f7355fa02966b2935fb1ec92ec87dfdc8
Giorgio Sironi <g.sironi@elifesciences.org>
2017-11-30 15:53:03 +0000

    Remove container on exit

--- project_tests.sh
+++ project_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-docker run elife/sciencebeam-gym /bin/bash -c 'venv/bin/pip install pytest nose && venv/bin/pytest sciencebeam_gym'
+docker run --rm elife/sciencebeam-gym /bin/bash -c 'venv/bin/pip install pytest nose && venv/bin/pytest sciencebeam_gym'
