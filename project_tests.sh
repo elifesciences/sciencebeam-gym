964599dbc3a502ed2a7c2c36ce16caffe33d634a
Giorgio Sironi <g.sironi@elifesciences.org>
2017-11-30 14:19:56 +0000

    Can now run all tests

--- project_tests.sh
+++ project_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-docker run elife/sciencebeam-gym /bin/bash -c 'venv/bin/pip install pytest nose && venv/bin/pytest sciencebeam_gym/preprocess'
+docker run elife/sciencebeam-gym /bin/bash -c 'venv/bin/pip install pytest nose && venv/bin/pytest sciencebeam_gym'
