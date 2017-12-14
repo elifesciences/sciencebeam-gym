fixed weighted cross entropy for softmax

sciencebeam_gym/trainer/models/pix2pix/pix2pix_core.py
@@ -388,7 +388,7 @@ def create_pix2pix_model(inputs, targets, a, pos_weight=None):
       pos_weight = tf.convert_to_tensor(pos_weight)
       gen_base_loss = weighted_cross_entropy_loss(
         logits=output_logits,
-        targets=targets,
+        labels=targets,
         pos_weight=pos_weight
       )
     else:

sciencebeam_gym/utils/tf.py
@@ -4,5 +4,5 @@ def variable_scoped(name, fn):
   with tf.variable_scope(name):
     return fn()
 
-def tf_print(x, message=None):
-  return tf.Print(x, [x], message=message)
+def tf_print(x, message=None, **kwargs):
+  return tf.Print(x, [x], message=message, **kwargs)