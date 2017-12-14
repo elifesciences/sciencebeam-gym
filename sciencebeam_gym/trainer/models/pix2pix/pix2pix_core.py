6fcdaa3bca3c185983d0d60fd2c10232c8f6b7bf
Daniel Ecer <de-code@users.noreply.github.com>
2017-12-14 14:51:32 +0000

    minor refactory of generator

--- sciencebeam_gym/trainer/models/pix2pix/pix2pix_core.py
+++ sciencebeam_gym/trainer/models/pix2pix/pix2pix_core.py
@@ -134,15 +134,101 @@ def deconv(batch_input, out_channels):
       padding="SAME"
     )
 
-def create_generator(generator_inputs, generator_outputs_channels, a, is_training):
+def create_encoder_layers(
+  generator_inputs,
+  layer_specs):
+  """
+  Creates encoders with every layer halfing width and height,
+  using an output depth specified by layer_specs.
+
+  Args:
+    generator_inputs: A `Tensor`. The input to the generator.
+    layer_specs: A list of number of output channels for each layer.
+  Returns:
+    A list of `Tensor`. The output tensor of each layer.
+  """
+
   layers = []
+  for out_channels in layer_specs:
+    with tf.variable_scope("encoder_%d" % (len(layers) + 1)):
+      if not layers:
+        # very first layer is connected to inputs and doesn't use batch norm
+        output = conv(generator_inputs, out_channels, stride=2)
+        layers.append(output)
+      else:
+        rectified = lrelu(layers[-1], 0.2)
+        # [batch, in_height, in_width, in_channels] =>
+        # [batch, in_height/2, in_width/2, out_channels]
+        convolved = conv(rectified, out_channels, stride=2)
+        output = batchnorm(convolved)
+        layers.append(output)
+  return layers
 
-    # encoder_1: [batch, 256, 256, in_channels] => [batch, 128, 128, ngf]
-  with tf.variable_scope("encoder_1"):
-    output = conv(generator_inputs, a.ngf, stride=2)
-    layers.append(output)
+def create_decoder_layers(
+  decoder_inputs,
+  layer_specs,
+  skip_connection_layers,
+  is_training):
+  """
+  Creates decoders with every layer doubling width and height,
+  using an output depth and dropout specified by layer_specs.
 
-  layer_specs = [
+  Args:
+    generator_inputs: A `Tensor`. The input to the generator.
+    layer_specs: A list of tuples for each layer. Each tuple containing
+      number of output channels and dropout.
+  Returns:
+    A list of `Tensor`. The output tensor of each layer.
+  """
+
+  layers = []
+  num_encoder_layers = len(skip_connection_layers)
+  for decoder_layer, (out_channels, dropout) in enumerate(layer_specs):
+    skip_layer = num_encoder_layers - decoder_layer - 1
+    is_last_layer = decoder_layer == len(layer_specs) - 1
+    with tf.variable_scope("decoder_%d" % (skip_layer + 1)):
+      if decoder_layer == 0:
+        # first decoder layer doesn't have skip connections
+        # since it is directly connected to the skip_layer
+        layer_input = decoder_inputs
+      else:
+        layer_input = tf.concat([layers[-1], skip_connection_layers[skip_layer]], axis=3)
+
+      rectified = tf.nn.relu(layer_input)
+      # [batch, in_height, in_width, in_channels] => [batch, in_height*2, in_width*2, out_channels]
+      output = deconv(rectified, out_channels)
+      if not is_last_layer:
+        # very last layer does not use batch norm
+        output = batchnorm(output)
+
+      if dropout > 0.0 and is_training:
+        # only apply dropout in training mode
+        output = tf.nn.dropout(output, keep_prob=1 - dropout)
+
+      layers.append(output)
+  return layers
+
+def create_encoder_decoder(
+  generator_inputs,
+  encoder_layer_specs,
+  decoder_layer_specs,
+  is_training):
+
+  encoder_layers = create_encoder_layers(
+    generator_inputs,
+    encoder_layer_specs
+  )
+  decoder_layers = create_decoder_layers(
+    encoder_layers[-1],
+    decoder_layer_specs,
+    encoder_layers,
+    is_training
+  )
+  return decoder_layers[-1]
+
+def create_generator(generator_inputs, generator_outputs_channels, a, is_training):
+  encoder_layer_specs = [
+    a.ngf,     # encoder_1: [batch, 256, 256, in_channels] => [batch, 128, 128, ngf]
     a.ngf * 2, # encoder_2: [batch, 128, 128, ngf] => [batch, 64, 64, ngf * 2]
     a.ngf * 4, # encoder_3: [batch, 64, 64, ngf * 2] => [batch, 32, 32, ngf * 4]
     a.ngf * 8, # encoder_4: [batch, 32, 32, ngf * 4] => [batch, 16, 16, ngf * 8]
@@ -151,16 +237,7 @@ def create_generator(generator_inputs, generator_outputs_channels, a, is_trainin
     a.ngf * 8, # encoder_7: [batch, 4, 4, ngf * 8] => [batch, 2, 2, ngf * 8]
     a.ngf * 8, # encoder_8: [batch, 2, 2, ngf * 8] => [batch, 1, 1, ngf * 8]
   ]
-
-  for out_channels in layer_specs:
-    with tf.variable_scope("encoder_%d" % (len(layers) + 1)):
-      rectified = lrelu(layers[-1], 0.2)
-      # [batch, in_height, in_width, in_channels] => [batch, in_height/2, in_width/2, out_channels]
-      convolved = conv(rectified, out_channels, stride=2)
-      output = batchnorm(convolved)
-      layers.append(output)
-
-  layer_specs = [
+  decoder_layer_specs = [
     (a.ngf * 8, 0.5),   # decoder_8: [batch, 1, 1, ngf * 8] => [batch, 2, 2, ngf * 8 * 2]
     (a.ngf * 8, 0.5),   # decoder_7: [batch, 2, 2, ngf * 8 * 2] => [batch, 4, 4, ngf * 8 * 2]
     (a.ngf * 8, 0.5),   # decoder_6: [batch, 4, 4, ngf * 8 * 2] => [batch, 8, 8, ngf * 8 * 2]
@@ -168,38 +245,15 @@ def create_generator(generator_inputs, generator_outputs_channels, a, is_trainin
     (a.ngf * 4, 0.0),   # decoder_4: [batch, 16, 16, ngf * 8 * 2] => [batch, 32, 32, ngf * 4 * 2]
     (a.ngf * 2, 0.0),   # decoder_3: [batch, 32, 32, ngf * 4 * 2] => [batch, 64, 64, ngf * 2 * 2]
     (a.ngf, 0.0),       # decoder_2: [batch, 64, 64, ngf * 2 * 2] => [batch, 128, 128, ngf * 2]
+    # decoder_1 [batch, 128, 128, ngf * 2] => [batch, 256, 256, generator_outputs_channels]
+    (generator_outputs_channels, 0.0)
   ]
-
-  num_encoder_layers = len(layers)
-  for decoder_layer, (out_channels, dropout) in enumerate(layer_specs):
-    skip_layer = num_encoder_layers - decoder_layer - 1
-    with tf.variable_scope("decoder_%d" % (skip_layer + 1)):
-      if decoder_layer == 0:
-        # first decoder layer doesn't have skip connections
-        # since it is directly connected to the skip_layer
-        layer_input = layers[-1]
-      else:
-        layer_input = tf.concat([layers[-1], layers[skip_layer]], axis=3)
-
-      rectified = tf.nn.relu(layer_input)
-      # [batch, in_height, in_width, in_channels] => [batch, in_height*2, in_width*2, out_channels]
-      output = deconv(rectified, out_channels)
-      output = batchnorm(output)
-
-      if dropout > 0.0 and is_training:
-        # only apply dropout in training mode
-        output = tf.nn.dropout(output, keep_prob=1 - dropout)
-
-      layers.append(output)
-
-  # decoder_1: [batch, 128, 128, ngf * 2] => [batch, 256, 256, generator_outputs_channels]
-  with tf.variable_scope("decoder_1"):
-    layer_input = tf.concat([layers[-1], layers[0]], axis=3)
-    rectified = tf.nn.relu(layer_input)
-    output = deconv(rectified, generator_outputs_channels)
-    layers.append(output)
-
-  return layers[-1]
+  return create_encoder_decoder(
+    generator_inputs,
+    encoder_layer_specs,
+    decoder_layer_specs,
+    is_training
+  )
 
 def create_discriminator(discrim_inputs, discrim_targets, a, out_channels=1):
   n_layers = 3
