872569cccf7905ea6dcf936ad225b1a76d50b37f
Daniel Ecer <de-code@users.noreply.github.com>
2018-01-19 16:42:59 +0000

    allow xml mapping filename to be overridden

--- preprocess.sh
+++ preprocess.sh
@@ -9,39 +9,35 @@ else
   CLOUD_ARGS=""
 fi
 
-python -m sciencebeam_gym.preprocess.preprocessing_pipeline \
-  --data-path="$DATA_SOURCE_PATH" \
-  --pdf-xml-file-list="$FILE_LIST_PATH/file-list-train.tsv" \
-  --limit=$TRAIN_FILE_LIMIT \
-  --output-path="${TRAIN_PREPROC_PATH}/" \
+COMMON_ARGS=(
   --annotation-evaluation-csv="annotation-evaluation.tsv" \
-  --xml-mapping-path=annot-xml-full.conf \
+  --xml-mapping-path="${XML_MAPPING_FILENAME}" \
   --min-annotation-percentage=$MIN_ANNOTATION_PERCENTAGE \
   --save-svg \
   --save-tfrecords \
   --image-width ${IMAGE_WIDTH} \
   --image-height ${IMAGE_HEIGHT} \
   --pages=$PAGE_RANGE \
-  --job-name-suffix=-train${DATASET_SUFFIX} \
   --num_workers=$NUM_WORKERS \
   $CLOUD_ARGS
+)
+
+
+python -m sciencebeam_gym.preprocess.preprocessing_pipeline \
+  --data-path="$DATA_SOURCE_PATH" \
+  --pdf-xml-file-list="$FILE_LIST_PATH/file-list-train.tsv" \
+  --limit=$TRAIN_FILE_LIMIT \
+  --output-path="${TRAIN_PREPROC_PATH}/" \
+  --job-name-suffix=-train${DATASET_SUFFIX} \
+  ${COMMON_ARGS[@]}
 
 python -m sciencebeam_gym.preprocess.preprocessing_pipeline \
   --data-path="$DATA_SOURCE_PATH" \
   --pdf-xml-file-list="$FILE_LIST_PATH/file-list-validation.tsv" \
   --limit=$EVAL_FILE_LIMIT \
   --output-path="${EVAL_PREPROC_PATH}/" \
-  --annotation-evaluation-csv="annotation-evaluation.tsv" \
-  --xml-mapping-path=annot-xml-full.conf \
-  --min-annotation-percentage=$MIN_ANNOTATION_PERCENTAGE \
-  --save-svg \
-  --save-tfrecords \
-  --image-width ${IMAGE_WIDTH} \
-  --image-height ${IMAGE_HEIGHT} \
-  --pages=$PAGE_RANGE \
   --job-name-suffix=-validation${DATASET_SUFFIX} \
-  --num_workers=$NUM_WORKERS \
-  $CLOUD_ARGS
+  ${COMMON_ARGS[@]}
 
 if [ ! -z "$QUALITATIVE_PREPROC_PATH" ]; then
   python -m sciencebeam_gym.preprocess.preprocessing_pipeline \
@@ -49,15 +45,6 @@ if [ ! -z "$QUALITATIVE_PREPROC_PATH" ]; then
     --pdf-xml-file-list="$FILE_LIST_PATH/file-list-validation.tsv" \
     --limit=$QUALITATIVE_FILE_LIMIT \
     --output-path="${QUALITATIVE_PREPROC_PATH}/" \
-    --annotation-evaluation-csv="annotation-evaluation.tsv" \
-    --xml-mapping-path=annot-xml-full.conf \
-    --min-annotation-percentage=$MIN_ANNOTATION_PERCENTAGE \
-    --save-svg \
-    --save-tfrecords \
-    --image-width ${IMAGE_WIDTH} \
-    --image-height ${IMAGE_HEIGHT} \
-    --pages=$PAGE_RANGE \
     --job-name-suffix=-qualitative${DATASET_SUFFIX} \
-    --num_workers=$NUM_WORKERS \
-    $CLOUD_ARGS
+    ${COMMON_ARGS[@]}
 fi
