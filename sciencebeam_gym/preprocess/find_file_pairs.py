9cbfacc0b5a88e8bdf567a0f22ffbed98181ddbb
Daniel Ecer <de-code@users.noreply.github.com>
2018-07-05 17:45:04 +0100

    renamed pdf_url to source_url

--- sciencebeam_gym/preprocess/find_file_pairs.py
+++ sciencebeam_gym/preprocess/find_file_pairs.py
@@ -29,8 +29,8 @@ def parse_args(argv=None):
     help='base data path'
   )
   parser.add_argument(
-    '--pdf-pattern', type=str, required=True,
-    help='pdf pattern'
+    '--source-pattern', type=str, required=True,
+    help='source pattern'
   )
   parser.add_argument(
     '--xml-pattern', type=str, required=True,
@@ -42,23 +42,30 @@ def parse_args(argv=None):
   )
   return parser.parse_args(argv)
 
-def main(argv=None):
-  args = parse_args(argv)
+
+def save_file_pairs_to_csv(output_path, source_xml_pairs):
+  mkdirs_if_not_exists(dirname(output_path))
+  delimiter = csv_delimiter_by_filename(output_path)
+  mime_type = 'text/tsv' if delimiter == '\t' else 'text/csv'
+  with FileSystems.create(output_path, mime_type=mime_type) as f:
+    writer = csv.writer(f, delimiter=delimiter)
+    write_csv_rows(writer, [['source_url', 'xml_url']])
+    write_csv_rows(writer, source_xml_pairs)
+  get_logger().info('written results to %s', output_path)
+
+def run(args):
   get_logger().info('finding file pairs')
-  pdf_xml_pairs = find_file_pairs_grouped_by_parent_directory_or_name([
-    join_if_relative_path(args.data_path, args.pdf_pattern),
+  source_xml_pairs = find_file_pairs_grouped_by_parent_directory_or_name([
+    join_if_relative_path(args.data_path, args.source_pattern),
     join_if_relative_path(args.data_path, args.xml_pattern)
   ])
-  pdf_xml_pairs = list(pdf_xml_pairs)
+  source_xml_pairs = list(source_xml_pairs)
 
-  mkdirs_if_not_exists(dirname(args.out))
-  delimiter = csv_delimiter_by_filename(args.out)
-  mime_type = 'text/tsv' if delimiter == '\t' else 'text/csv'
-  with FileSystems.create(args.out, mime_type=mime_type) as f:
-    writer = csv.writer(f, delimiter=delimiter)
-    write_csv_rows(writer, [['pdf_url', 'xml_url']])
-    write_csv_rows(writer, pdf_xml_pairs)
-  get_logger().info('written results to %s', args.out)
+  save_file_pairs_to_csv(args.out, source_xml_pairs)
+
+def main(argv=None):
+  args = parse_args(argv)
+  run(args)
 
 if __name__ == '__main__':
   logging.basicConfig(level='INFO')
---
+++ sciencebeam_gym/preprocess/find_file_pairs_test.py
@@ -0,0 +1,106 @@
+import logging
+import os
+from mock import patch
+
+import pytest
+
+import sciencebeam_gym.preprocess.find_file_pairs as find_file_pairs
+from sciencebeam_gym.preprocess.find_file_pairs import (
+  run,
+  parse_args,
+  main
+)
+
+
+LOGGER = logging.getLogger(__name__)
+
+BASE_SOURCE_PATH = '/source'
+
+PDF_FILE_1 = BASE_SOURCE_PATH + '/file1.pdf'
+XML_FILE_1 = BASE_SOURCE_PATH + '/file1.xml'
+PDF_FILE_2 = BASE_SOURCE_PATH + '/file2.pdf'
+XML_FILE_2 = BASE_SOURCE_PATH + '/file2.xml'
+
+SOURCE_PATTERN = '*.pdf'
+XML_PATTERN = '*.xml'
+OUTPUT_FILE = 'file-list.tsv'
+
+SOME_ARGV = [
+  '--data-path=%s' % BASE_SOURCE_PATH,
+  '--source-pattern=%s' % SOURCE_PATTERN,
+  '--xml-pattern=%s' % XML_PATTERN,
+  '--out=%s' % OUTPUT_FILE
+]
+
+
+@pytest.fixture(name='find_file_pairs_grouped_by_parent_directory_or_name_mock')
+def _find_file_pairs_grouped_by_parent_directory_or_name():
+  with patch.object(find_file_pairs, 'find_file_pairs_grouped_by_parent_directory_or_name') as m:
+    yield m
+
+@pytest.fixture(name='save_file_pairs_to_csv_mock')
+def _save_file_pairs_to_csv():
+  with patch.object(find_file_pairs, 'save_file_pairs_to_csv') as m:
+    yield m
+
+@pytest.fixture(name='save_file_pairs_to_csv_mock')
+def _save_file_pairs_to_csv():
+  with patch.object(find_file_pairs, 'save_file_pairs_to_csv') as m:
+    yield m
+
+@pytest.fixture(name='parse_args_mock')
+def _parse_args():
+  with patch.object(find_file_pairs, 'parse_args') as m:
+    yield m
+
+@pytest.fixture(name='run_mock')
+def _run():
+  with patch.object(find_file_pairs, 'run') as m:
+    yield m
+
+class TestRun(object):
+  def test_should_pass_around_parameters(
+    self,
+    find_file_pairs_grouped_by_parent_directory_or_name_mock,
+    save_file_pairs_to_csv_mock):
+
+    opt = parse_args(SOME_ARGV)
+    find_file_pairs_grouped_by_parent_directory_or_name_mock.return_value = [
+      (PDF_FILE_1, XML_FILE_1),
+      (PDF_FILE_2, XML_FILE_2)
+    ]
+    run(opt)
+    find_file_pairs_grouped_by_parent_directory_or_name_mock.assert_called_with([
+      os.path.join(BASE_SOURCE_PATH, SOURCE_PATTERN),
+      os.path.join(BASE_SOURCE_PATH, XML_PATTERN)
+    ])
+    save_file_pairs_to_csv_mock.assert_called_with(
+      opt.out,
+      find_file_pairs_grouped_by_parent_directory_or_name_mock.return_value
+    )
+
+  def test_should_generate_file_list(self, tmpdir):
+    data_path = tmpdir.join(BASE_SOURCE_PATH)
+    pdf_file_1 = tmpdir.join(PDF_FILE_1)
+    xml_file_1 = tmpdir.join(XML_FILE_1)
+    out_file = tmpdir.join(OUTPUT_FILE)
+    LOGGER.debug('pdf_file_1: %s, xml_file: %s', pdf_file_1, xml_file_1)
+    data_path.mkdir()
+    pdf_file_1.write(b'')
+    xml_file_1.write(b'')
+    opt = parse_args(SOME_ARGV)
+    opt.data_path = str(data_path)
+    opt.out = str(out_file)
+    run(opt)
+    out_lines = [s.strip() for s in tmpdir.join(OUTPUT_FILE).read().strip().split('\n')]
+    LOGGER.debug('out_lines: %s', out_lines)
+    assert out_lines == [
+      'source_url\txml_url',
+      '%s\t%s' % (pdf_file_1, xml_file_1)
+    ]
+
+class TestMain(object):
+  def test_should_parse_args_and_call_run(self, parse_args_mock, run_mock):
+    main(SOME_ARGV)
+    parse_args_mock.assert_called_with(SOME_ARGV)
+    run_mock.assert_called_with(parse_args_mock.return_value)
