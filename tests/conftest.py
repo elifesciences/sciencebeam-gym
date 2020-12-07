6ecc99fc793e5ab181430443dffe8fda61fc8834
Daniel Ecer <de-code@users.noreply.github.com>
2020-12-07 16:51:34 +0000

    configured test logging

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,7 @@ import pytest
 
 @pytest.fixture(scope='session', autouse=True)
 def setup_logging():
-    logging.root.handlers = []
-    logging.basicConfig(level='WARNING')
-    logging.getLogger('sciencebeam_gym').setLevel('DEBUG')
+    logging.root.setLevel('WARNING')
+    for name in ['sciencebeam_utils', 'sciencebeam_alignment', 'sciencebeam_gym']:
+        logging.getLogger(name).setLevel('DEBUG')
     logging.getLogger('tensorflow').setLevel('ERROR')
