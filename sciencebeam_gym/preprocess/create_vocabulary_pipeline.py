46b6f74206ddb9ad12900e48968d2650a1c2e4ad
Daniel Ecer <de-code@users.noreply.github.com>
2021-05-17 11:33:58 +0100

    initial create vocabulary utility

---
+++ sciencebeam_gym/preprocess/create_vocabulary_pipeline.py
@@ -0,0 +1,57 @@
+import argparse
+import logging
+from collections import Counter
+from typing import Iterable, List, Optional
+
+from lxml import etree
+
+import pandas as pd
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+def parse_args(argv: Optional[List[str]] = None) -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--input-file',
+        type=str,
+        required=True
+    )
+    parser.add_argument(
+        '--word-count-file',
+        type=str,
+        required=True
+    )
+    return parser.parse_args(argv)
+
+
+def iter_tokens_from_xml_root(
+    xml_root: etree.ElementBase
+) -> Iterable[str]:
+    for text in xml_root.itertext():
+        for token in text.strip().split(' '):
+            yield token
+
+
+def iter_tokens_from_xml_file(xml_file: str) -> Iterable[str]:
+    yield from iter_tokens_from_xml_root(
+        etree.parse(xml_file).getroot()
+    )
+
+
+def run(args: argparse.Namespace):
+    LOGGER.info('args=%r', args)
+    word_counts = Counter(iter_tokens_from_xml_file(args.input_file))
+    word_count_df = pd.DataFrame(
+        {
+            'token': key,
+            'count': value
+        } for key, value in word_counts.items()
+    )
+    word_count_df.to_csv(args.word_count_file, sep='\t', index=False)
+
+
+def main(argv: Optional[List[str]] = None):
+    args = parse_args(argv)
+    run(args)
---
+++ tests/preprocess/create_vocabulary_pipeline_test.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+
+import pandas as pd
+
+from sciencebeam_gym.preprocess.create_vocabulary_pipeline import (
+    main
+)
+
+
+class TestMainEndToEnd:
+    def test_should_extract_vocabulary_from_single_xml_file(self, tmp_path: Path):
+        xml_file = tmp_path / 'test.xml'
+        xml_file.write_text(
+            '''
+            <xml>
+            <item>some text</item>
+            <item>more text</item>
+            </xml>
+            '''
+        )
+        word_count_file = tmp_path / 'word-count.tsv'
+        main([
+            f'--input-file={xml_file}',
+            f'--word-count-file={word_count_file}'
+        ])
+        assert word_count_file.exists()
+        df = pd.read_csv(word_count_file, sep='\t')
+        assert df.groupby('token').sum()['count'].to_dict() == {
+            'text': 2,
+            'some': 1,
+            'more': 1
+        }
