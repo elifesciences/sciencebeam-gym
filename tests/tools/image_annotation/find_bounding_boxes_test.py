0df386fc2099769f119d4145bacc6f78ea03631d
Daniel Ecer <de-code@users.noreply.github.com>
2021-08-31 14:49:35 +0100

    added test for smaller partial image

--- tests/tools/image_annotation/find_bounding_boxes_test.py
+++ tests/tools/image_annotation/find_bounding_boxes_test.py
@@ -3,16 +3,41 @@ import logging
 from pathlib import Path
 
 import PIL.Image
+import numpy as np
+from cv2 import cv2 as cv
 from sklearn.datasets import load_sample_image
 
+from sciencebeam_gym.utils.bounding_box import BoundingBox
 from sciencebeam_gym.tools.image_annotation.find_bounding_boxes import main
 
 
 LOGGER = logging.getLogger(__name__)
 
 
+def resize_image(src: np.ndarray, width: int, height: int) -> np.ndarray:
+    return cv.resize(
+        src,
+        dsize=(width, height),
+        interpolation=cv.INTER_CUBIC
+    )
+
+
+def copy_image_to(
+    src: np.ndarray,
+    dst: np.ndarray,
+    dst_bounding_box: BoundingBox,
+):
+    x = int(dst_bounding_box.x)
+    y = int(dst_bounding_box.y)
+    width = int(dst_bounding_box.width)
+    height = int(dst_bounding_box.height)
+    dst[y:(y + height), x:(x + width)] = resize_image(
+        src, width=width, height=height
+    )
+
+
 class TestMain:
-    def test_should_annotate_image(self, tmp_path: Path):
+    def test_should_annotate_single_full_page_image(self, tmp_path: Path):
         sample_image = load_sample_image('flower.jpg')
         LOGGER.debug('sample_image: %s (%s)', sample_image.shape, sample_image.dtype)
         image_path = tmp_path / 'test.jpg'
@@ -55,4 +80,47 @@ class TestMain:
         assert annotation_json['bbox'] == [
             0, 0, image_json['width'], image_json['height']
         ]
-        # assert False
+
+    def test_should_annotate_smaller_image(self, tmp_path: Path):
+        sample_image = load_sample_image('flower.jpg')
+        LOGGER.debug('sample_image: %s (%s)', sample_image.shape, sample_image.dtype)
+        image_path = tmp_path / 'test.jpg'
+        pdf_path = tmp_path / 'test.pdf'
+        output_json_path = tmp_path / 'test.json'
+        pdf_page_image = np.zeros((400, 600, 3), dtype=np.uint8)
+        copy_image_to(
+            sample_image,
+            pdf_page_image,
+            BoundingBox(20, 30, 240, 250),
+        )
+        PIL.Image.fromarray(sample_image).save(image_path, 'JPEG')
+        PIL.Image.fromarray(pdf_page_image).save(
+            pdf_path,
+            'PDF',
+            resolution=100.0,
+            save_all=True,
+            append_images=[]
+        )
+        main([
+            '--pdf-file',
+            str(pdf_path),
+            '--image-file',
+            str(image_path),
+            '--output-json-file',
+            str(output_json_path)
+        ])
+        assert output_json_path.exists()
+        json_data = json.loads(output_json_path.read_text())
+        LOGGER.debug('json_data: %s', json_data)
+        images_json = json_data['images']
+        assert len(images_json) == 1
+        image_json = images_json[0]
+        assert image_json['width'] == 1200
+        assert image_json['height'] == 800
+        annotations_json = json_data['annotations']
+        annotation_json = annotations_json[0]
+        np.testing.assert_allclose(
+            annotation_json['bbox'],
+            [40, 60, 480, 500],
+            atol=10
+        )
