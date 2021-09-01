873ba9cfd14a0b13731d9c473d67d67856aff7d1
Daniel Ecer <de-code@users.noreply.github.com>
2021-09-01 16:03:21 +0100

    added TestGetObjectMatch

---
+++ tests/utils/image_object_matching_test.py
@@ -0,0 +1,37 @@
+import numpy as np
+import PIL.Image
+import pytest
+from sklearn.datasets import load_sample_image
+
+from sciencebeam_gym.utils.image_object_matching import (
+    get_sift_detector_matcher,
+    get_bounding_box_for_points,
+    get_object_match
+)
+
+
+@pytest.fixture(name='sample_image_array', scope='session')
+def _sample_image_array() -> np.ndarray:
+    return load_sample_image('flower.jpg')
+
+
+class TestGetObjectMatch:
+    def test_should_match_full_size_image(
+        self,
+        sample_image_array: np.ndarray
+    ):
+        height, width = sample_image_array.shape[:2]
+        object_detector_matcher = get_sift_detector_matcher()
+        bounding_box = get_bounding_box_for_points(
+            get_object_match(
+                object_detector_matcher,
+                PIL.Image.fromarray(sample_image_array),
+                PIL.Image.fromarray(sample_image_array)
+            )[0]
+        )
+        assert bounding_box
+        np.testing.assert_allclose(
+            bounding_box.to_list(),
+            [0, 0, width, height],
+            atol=10
+        )
