122b098363ee699fbaa889f344c0f216ddd4a9a9
Daniel Ecer <de-code@users.noreply.github.com>
2021-10-18 21:09:50 +0100

    increased version to 0.0.2

--- sciencebeam_gym/tools/image_annotation/find_bounding_boxes_utils.py
+++ sciencebeam_gym/tools/image_annotation/find_bounding_boxes_utils.py
@@ -616,7 +616,7 @@ def process_single_document(
         )
     data_json = {
         'info': {
-            'version': '0.0.1',
+            'version': '0.0.2',
             'date_created': datetime.utcnow().isoformat()
         },
         'images': [
