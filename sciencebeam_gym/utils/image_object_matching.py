3ad654e08775e0c0cdd256753e14093bb5a42d44
Daniel Ecer <de-code@users.noreply.github.com>
2021-10-21 09:44:55 +0100

    further improve bounding box matching (#435)
    * log keypoint match score
    
    * reduced default min keypoint match score
    
    * reduced the default min keypoint match to 0.01

--- sciencebeam_gym/utils/image_object_matching.py
+++ sciencebeam_gym/utils/image_object_matching.py
@@ -27,7 +27,7 @@ DEFAULT_MAX_BOUNDING_BOX_ADJUSTMENT_ITERATIONS = 0
 
 DEFAULT_USE_CANNY = False
 
-MIN_KEYPOINT_MATCH_SCORE = 0.5
+MIN_KEYPOINT_MATCH_SCORE = 0.01
 MIN_TEMPLATE_MATCH_SCORE = 0.6
 
 
@@ -793,9 +793,11 @@ def iter_current_best_image_list_object_match(
         ):
             _template_image_id = kwargs.get('template_image_id')
             LOGGER.info(
-                'no object match found, falling back to template matching'
-                ' (this might take a while): %r',
-                _template_image_id
+                'no keypoint match found, falling back to template matching'
+                ' (this might take a while): %r (%r < %r)',
+                _template_image_id,
+                best_image_list_object_match.score,
+                min_keypoint_match_score
             )
             best_image_list_object_match = get_best_image_list_object_match(
                 iter_image_list_template_match(
